ef829cbbf9a40d3941f9437e3d797ef24ab9cd4e
Japhet Moise <123456789+odin@users.noreply.github.com>
2023-07-22 22:30:37 +0200

    Pushing of main 4  files and their main scripts(codes)

---
+++ script.js
@@ -0,0 +1,8 @@
+let add=(term1,term2)=>term1+term2;
+let substract=(term1,term2)=>term1-term2;
+let divide=(term1,term2)=>term1/term2;
+let multiply=(term1,term2)=>term1*term2;
+let data=0;
+function display(){
+    document.querySelector("#result").innerHTML="0000000";
+}
